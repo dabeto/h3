2d69001c2215fad6026d29f1363779b1d584ae44
Daniel <daniel@rodriguezmendez.com>
2018-09-09 09:07:07 -0500

    äsdf

--- ext/h3/extconf.rb
+++ ext/h3/extconf.rb
@@ -1,17 +1,7 @@
 require "mkmf"
 
-HEADER_DIRS = [
-  ".",
-  "./h3lib/include"
-]
-LIB_DIRS = [
-  ".",
-  "./h3lib/lib"
-]
-
-dir_config("h3", HEADER_DIRS, LIB_DIRS)
 
 have_header("h3api.h")or raise
-have_library('h3') or raise
+#have_library('h3') or raise
 
 create_makefile('h3/h3')
--- ext/h3/stackAlloc.h
+++ ext/h3/stackAlloc.h
@@ -57,8 +57,6 @@
 #else
 
 
-
-
 #endif
 
 #endif
