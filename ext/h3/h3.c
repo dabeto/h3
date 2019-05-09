61552d111151d2391d14814bca22c3c8225a73a8
jssilva93 <jssilva93@gmail.com>
2019-05-09 12:43:10 -0500

    test

--- ext/h3/h3.c
+++ ext/h3/h3.c
@@ -147,5 +147,5 @@ Init_h3(void) {
   rb_define_method(cMyMalloc, "geo_to_h3", geo_to_h3, 1);
   rb_define_method(cMyMalloc, "h3_to_geo_boundary", h3_to_geo_boundary, 1);
   rb_define_method(cMyMalloc, "h3_to_geo", h3_to_geo, 1);
-  rb_define_method(cMyMalloc, "neighbors", neighbors, 1);
+  rb_define_method(cMyMalloc, "neighbors", neighbors, 2);
 }
