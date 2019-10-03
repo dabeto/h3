2a6ebabc0cf2cb27f997af8e84f2638e6a41274b
jssilva93 <jssilvaf@poligran.edu.co>
2019-10-03 02:36:02 -0500

    polyfill

--- ext/h3/h3.c
+++ ext/h3/h3.c
@@ -146,7 +146,7 @@ static VALUE h3_to_geo_boundary(VALUE self, VALUE h3) {
       // Process the array
       //unsigned int array_size = (unsigned int)RARRAY_LEN(v_array);
       for (unsigned int i = 0; i < 1; ++i) {
-          //VALUE v_internal_array = rb_ary_entry(v_array, i);
+          VALUE v_internal_array = rb_ary_entry(v_array, i);
           // Ensure the internal value is an array
           //Check_Type(v_internal_array, T_ARRAY);
           // Process the internal array
