7a5cfb513469a7668abec6d8965c58c00e9d99a2
jssilva93 <jssilvaf@poligran.edu.co>
2019-10-03 03:18:10 -0500

    polyfill

--- ext/h3/h3.c
+++ ext/h3/h3.c
@@ -174,22 +174,9 @@ static VALUE h3_to_geo_boundary(VALUE self, VALUE h3) {
     polyfill(multipolygon, resolution, neighboring);
 
 
-      VALUE r_array = rb_ary_new2(maxNeighboring);
-            //printf("Neighbors:\n");
-            for (int i = 0; i < maxNeighboring; i++) {
-                // Some indexes may be 0 to indicate fewer than the maximum
-                // number of indexes.
-                if (neighboring[i] != 0) {
-                  char str[20];
-                  sprintf(str, "%" PRIx64, neighboring[i]);
-                  rb_ary_push(r_array, rb_str_new2(str));
-                    //printf("%" PRIx64 "\n", neighboring[i]);
-                }
-            }
 
-            free(neighboring);
 
-            return r_array;
+            return Qnil;
 
   }
 
