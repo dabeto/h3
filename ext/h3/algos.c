b10acea44d351d215efc44e7749fb8f9e161df26
jssilva93 <jssilvaf@poligran.edu.co>
2019-10-03 00:06:42 -0500

    test

--- ext/h3/algos.c
+++ ext/h3/algos.c
@@ -807,12 +807,5 @@ void H3_EXPORT(h3SetToLinkedGeo)(const H3Index* h3Set, const int numHexes,
 }
 
 Init_algos(void) {
-  VALUE cMyMalloc;
-
-  cMyMalloc = rb_define_class("H3", rb_cObject);
-
-  rb_define_alloc_func(cMyMalloc, my_malloc_alloc);
-  rb_define_method(cMyMalloc, "initialize", my_malloc_init, 0);
-  rb_define_method(cMyMalloc, "free", my_malloc_release, 0);
   rb_define_method(cMyMalloc, "polyfill", polyfill, 3);
 }
